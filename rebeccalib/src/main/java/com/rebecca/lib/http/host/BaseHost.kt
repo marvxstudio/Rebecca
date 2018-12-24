d916395d7da409c5ddc8b5c6841a5ae8c69b0050
marvxstudio <marvxstudio@outlook.com>
2018-12-24 11:00:56 +0800

    commit

--- rebeccalib/src/main/java/com/rebecca/lib/http/host/BaseHost.kt
+++ rebeccalib/src/main/java/com/rebecca/lib/http/host/BaseHost.kt
@@ -1,12 +1,13 @@
 package com.rebecca.lib.http.host
 
-abstract class BaseHost(var ip: String, var portHttp: String = "80", var portHttps: String = "443") {
+abstract class BaseHost(var ip: String, var portHttp: String = "80", var portHttps: String = "443", val urlHttp: String =
+        "$HTTP$ip:$portHttp", val urlHttps: String = "$HTTPS$ip:$portHttps") {
 
     //========================================================
 
-    open val urlHttp: String = "$HTTP$ip:$portHttp"
+    //open val urlHttp: String = "$HTTP$ip:$portHttp"
 
-    open val urlHttps: String = "$HTTPS$ip:$portHttps"
+    //open val urlHttps: String = "$HTTPS$ip:$portHttps"
 
     //========================================================
     open var url: String = urlHttps
