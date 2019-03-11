f0d0dff3fdff711e1c44ce69cab3992d64df843f
marvxstudio <marvxstudio@outlook.com>
2019-03-11 15:07:18 +0800

    commit

--- rebeccalib/src/main/java/com/rebecca/lib/zbase/application/BaseVMApplication.kt
+++ rebeccalib/src/main/java/com/rebecca/lib/zbase/application/BaseVMApplication.kt
@@ -2,10 +2,13 @@ package com.rebecca.lib.zbase.application
 
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
+import com.rebecca.lib.tools.AppTools
 import com.rebecca.lib.tools.Loger
 
 open class BaseVMApplication : Application() {
 
+  //=========================  =================================
+  val isAppDebug: Boolean by lazy { AppTools.isAppDebug(this) }
   //=========================  =================================
   open var openRouter = false
   open var openRouterDebug = false
@@ -19,6 +22,8 @@ open class BaseVMApplication : Application() {
       ARouter.openLog()
     }
   }
+  //=========================  =================================
+
   //========================= init  =================================
 
   //========================= main ==================================
