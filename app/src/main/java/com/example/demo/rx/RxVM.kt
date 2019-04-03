bc3ed9710316a22b625cbe7aa716469223275d09
marvxstudio <marvxstudio@outlook.com>
2019-04-03 18:01:35 +0800

    commit

--- app/src/main/java/com/example/demo/rx/RxVM.kt
+++ app/src/main/java/com/example/demo/rx/RxVM.kt
@@ -1,8 +1,9 @@
 package com.example.demo.rx
 
+import android.arch.lifecycle.MediatorLiveData
 import com.rebecca.lib.zbase.vm.BaseVM
 
 class RxVM : BaseVM() {
 
-  var text = "123"
+  val text = MediatorLiveData<String>().also { it.value = "RX" }
 }
