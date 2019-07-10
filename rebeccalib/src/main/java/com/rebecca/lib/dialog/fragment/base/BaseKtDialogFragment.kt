4b55c717d9bc430729ac37dda2a2d9ee42be6fa6
marvxstudio <marvxstudio@outlook.com>
2019-07-10 15:53:01 +0800

    commit

--- rebeccalib/src/main/java/com/rebecca/lib/dialog/fragment/base/BaseKtDialogFragment.kt
+++ rebeccalib/src/main/java/com/rebecca/lib/dialog/fragment/base/BaseKtDialogFragment.kt
@@ -1,5 +1,6 @@
 package com.rebecca.lib.dialog.fragment.base
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.content.DialogInterface.OnDismissListener
 import android.graphics.Color
@@ -16,84 +17,93 @@ import com.rebecca.lib.dialog.IDialogEnterClicker
 import com.rebecca.lib.zbase.ICreate
 
 abstract class BaseKtDialogFragment : DialogFragment(), ICreate {
-    //=========================  =================================
-    val mTag = javaClass.simpleName
-    open var isDebug = false
-    //=========================  =================================
-    open var openTitle = true
+  //=========================  =================================
+  val mTag = javaClass.simpleName
+  open var isDebug = false
+  //=========================  =================================
+  open var openTitle = true
 
-    open var openBackground = true
+  open var openBackground = true
 
-    open var openTouchOutside = true
+  open var openTouchOutside = true
 
-    open var widthPer = 0.75
-    //=========================  =================================
-    var mRootView: View? = null
+  open var widthPCT = 0.75
+  //=========================  =================================
+  var mRootView: View? = null
 
-    var clickerCancel: IDialogCanceClicker? = null
+  var clickerCancel: IDialogCanceClicker? = null
 
-    var clickerEnter: IDialogEnterClicker? = null
-    //========================= init  =================================
-    override fun onInit() {
-        onInitView()
-        onInitData()
+  var clickerEnter: IDialogEnterClicker? = null
+  //=========================  =================================
+  //=========================  =================================
+  open fun onInitDialog() {
+    if (openTitle == false) {
+      dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
     }
-
-    override fun onInitData() {
+    if (openBackground == false) {
+      dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
     }
+    dialog.setCanceledOnTouchOutside(openTouchOutside)
+  }
 
-    override fun onInitView() {
-        if (openTitle == false) {
-            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        }
-        if (openBackground == false) {
-            dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
-        dialog.setCanceledOnTouchOutside(openTouchOutside)
+  open fun onInitWindow(dialog: Dialog? = this.dialog, window: Window? = dialog?.window) {
+    window?.let {
+      if (openTitle == false) {
+        val dm = DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(dm)
+        val width: Int = (dm.widthPixels * widthPCT).toInt()
+        it.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
+      }
     }
+  }
 
-    open fun createRootView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  //=========================  =================================
+  fun add(clicker: IDialogEnterClicker?): BaseKtDialogFragment {
+    clickerEnter = clicker
+    return this
+  }
 
-        return null
+  fun add(clicker: IDialogCanceClicker?): BaseKtDialogFragment {
+    clickerCancel = clicker
+    return this
+  }
+
+  //========================= init  =================================
+  override fun onInit() {
+    onInitView()
+    onInitData()
+  }
+
+  override fun onInitData() {
+  }
+
+  override fun onInitView() {
+    onInitDialog()
+  }
+
+  open fun createRootView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    return null
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    mRootView = createRootView(inflater, container, savedInstanceState)
+    onInit()
+    return mRootView
+  }
+
+  override fun onDismiss(dialog: DialogInterface?) {
+    super.onDismiss(dialog)
+    if (activity is OnDismissListener) {
+      val listener = activity as OnDismissListener
+      listener.onDismiss(dialog)
     }
+  }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mRootView = createRootView(inflater, container, savedInstanceState)
-        onInit()
-        return mRootView
-    }
+  //========================= main ==================================
+  override fun onStart() {
+    super.onStart()
+    onInitWindow()
+  }
 
-    override fun onDismiss(dialog: DialogInterface?) {
-        super.onDismiss(dialog)
-        if (activity is OnDismissListener) {
-            val listener = activity as OnDismissListener
-            listener.onDismiss(dialog)
-        }
-    }
-
-    open fun onInitWindow() {
-        if (dialog != null && openTitle == false) {
-            val dm = DisplayMetrics()
-            activity?.windowManager?.defaultDisplay?.getMetrics(dm)
-            val width: Int = (dm.widthPixels * widthPer).toInt()
-            dialog.getWindow()?.setLayout(width, ViewGroup.LayoutParams.WRAP_CONTENT)
-        }
-    }
-
-    //========================= main ==================================
-    override fun onStart() {
-        super.onStart()
-        onInitWindow()
-    }
-
-    //=========================  =================================
-    fun add(clicker: IDialogEnterClicker?): BaseKtDialogFragment {
-        clickerEnter = clicker
-        return this
-    }
-
-    fun add(clicker: IDialogCanceClicker?): BaseKtDialogFragment {
-        clickerCancel = clicker
-        return this
-    }
 }
