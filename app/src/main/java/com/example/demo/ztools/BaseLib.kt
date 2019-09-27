daa1b8be6f1d1f6c8c4bc90ff7b95716113c1858
marvxstudio <marvxstudio@outlook.com>
2019-09-27 16:44:50 +0800

    commit

---
+++ app/src/main/java/com/example/demo/ztools/BaseLib.kt
@@ -0,0 +1,12 @@
+package com.example.demo.ztools
+
+import android.widget.Button
+import android.widget.TextView
+
+class BaseLib {
+  fun kk() {
+    var listS: ArrayList<out TextView> = ArrayList<TextView>()
+    var listkk = ArrayList<Button>()
+    listS = listkk
+  }
+}
