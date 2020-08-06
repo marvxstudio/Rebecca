89e8027d355383cc620d4876a7c52e2f53b6276c
marvxstudio <marvxstudio@outlook.com>
2020-08-06 16:01:19 +0800

    commit

--- rebeccalib/src/main/java/com/rebecca/lib/http/client/BaseHttpClient.kt
+++
@@ -1,8 +0,0 @@
-package com.rebecca.lib.http.client
-
-import com.rebecca.lib.http.BaseHttper
-
-abstract class BaseHttpClient<API : Any>(httper: BaseHttper, var clsApi: Class<API>) {
-
-    val api: API = httper.createApi(clsApi)
-}
